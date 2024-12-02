a7084a2f05be9a98c2dc9c4b9f917a4d56bbed35
Eliessez <eliesse.zine75@gmail.com>
2024-12-02 13:25:54 +0100

    configdeploiement

---
+++ migrations/Version20241202121210.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241202121210 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE annonce CHANGE numero_annonce numero_annonce VARCHAR(13) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F65593E530018D44 ON annonce (numero_annonce)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_F65593E530018D44 ON annonce');
+        $this->addSql('ALTER TABLE annonce CHANGE numero_annonce numero_annonce VARCHAR(255) NOT NULL');
+    }
+}
